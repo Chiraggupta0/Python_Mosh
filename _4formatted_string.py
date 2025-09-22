f7823972eb8350e8f0acb70ba32c46cd4cf947e0
CHIRAG GUPTA <chiraggupta0963@gmail.com>
2025-09-22 15:05:43 +0530

    updated

---
+++ _4formatted_string.py
@@ -0,0 +1,9 @@
+first="chirag"
+last="gupta"
+full=first+" "+last
+print(full)
+#  we can use format string as well as:--
+formatt=f"{first}  {last}"
+print(formatt)
+formatt=f"{len(first)} {last} {2*2}"
+print(formatt)
