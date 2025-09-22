eec5b0c03ace61ffeee084e6b586e6ed674e5404
CHIRAG GUPTA <chiraggupta0963@gmail.com>
2025-09-22 15:05:01 +0530

    updated

---
+++ _2string.py
@@ -0,0 +1,14 @@
+course="python programming"
+message="""
+hi john,
+my name is chirag
+"""
+print(course)
+print(message)
+
+print(len(message))
+print(course[0])
+print(course[0:])
+print(course[0:3])
+print(course[:3])
+print(course[:])
