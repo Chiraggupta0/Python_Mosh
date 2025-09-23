f48e70de057d2bdc3cf26787cb9154c00b85197e
CHIRAG GUPTA <chiraggupta0963@gmail.com>
2025-09-23 15:58:29 +0530

    updated

---
+++ _17while_loop.py
@@ -0,0 +1,15 @@
+number=100
+while number>0:
+    print(number)
+    # number-=1
+    number//=2
+
+
+
+command=""
+while command !="quit" and command!="QUIT":
+    command=input(">")
+    print("ECHO",command)
+ 
+#  this while loop will run till i type quit or QUIT
+#  it will keep taking input and will keep displaying it till i type quit
