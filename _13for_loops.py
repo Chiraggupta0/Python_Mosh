bbb901c56dbaf9e420f6d77de01f9a4eb312fc83
CHIRAG GUPTA <chiraggupta0963@gmail.com>
2025-09-23 15:23:27 +0530

    updated

---
+++ _13for_loops.py
@@ -0,0 +1,7 @@
+for number in range(3):
+    print("sending a message", number+1,(number+1)*".")
+#  in for loop number is an integer like in java we write i=0;i<n;i++ here that i is number range(3) defines the no of times this loop will run
+
+#  if want to start loop from 1 and end till 10 and want to skip 2 each time then we can code like this
+for number in range(1,10,2):
+    print("sending a message", number,(number)*".")
