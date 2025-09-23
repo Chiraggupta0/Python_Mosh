2aa77bb9706c371bf325a76a4d973f72c6e27692
CHIRAG GUPTA <chiraggupta0963@gmail.com>
2025-09-23 16:09:44 +0530

    updated

---
+++ _18infinite_loop.py
@@ -0,0 +1,6 @@
+while True:
+    command=input(">")
+    print("ECHO", command)
+    if command.lower() == "quit":
+        break
+#  here while is using a infinite loop and to break that we are defining a condition and if it is true it will break the loop
